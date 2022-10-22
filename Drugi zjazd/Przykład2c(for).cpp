495b50978e1db6adcad05312b50069bc7947bec7
Jakub Pląsek <116102063+jakubplasek@users.noreply.github.com>
2022-10-22 18:16:43 +0200

    Add files via upload

---
+++ Drugi zjazd/Przykład2c(for).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int loop_counter;
+
+    for (loop_counter = 1; loop_counter < 10; loop_counter++)
+    {
+        cout << loop_counter << " Loop situation" << endl;
+        
+    }
+
+
+
+
+    return 0;
+}
