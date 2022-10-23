1fea340983aef230ab5883f8b8d8ae38eeaf5955
Jakub Pląsek <116102063+jakubplasek@users.noreply.github.com>
2022-10-23 15:51:09 +0200

    Add files via upload

---
+++ Week 2/Przykład.2e(break).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 0;
+    while(i < 16)
+    {
+        cout << i << endl;
+        i++;
+        if (i == 11)
+        {
+        break;
+        }
+    }
+
+
+
+
+
+
+
+    return 0;
+}
