f90f04125e8688015761d03596e239d5989c36bd
Jakub Pląsek <116102063+jakubplasek@users.noreply.github.com>
2022-11-05 19:35:00 +0100

    Add files via upload

---
+++ Week 3/Zadanie 3.3.cpp
@@ -0,0 +1,60 @@
+// C++ program to find roots of a quadratic equation
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Prints roots of quadratic equation ax*2 + bx + x
+void findRoots(int a, int b, int c)
+{
+    // If a is 0, then equation is not quadratic, but
+    // linear
+    if (a == 0) {
+        cout << "Invalid";
+        return;
+    }
+
+    int d = b * b - 4 * a * c;
+    double sqrt_val = sqrt(abs(d));
+
+    if (d > 0) {
+        cout << "Roots are real and different \n";
+        cout << (double)(-b + sqrt_val) / (2 * a) << "\n"
+             << (double)(-b - sqrt_val) / (2 * a);
+    }
+    else if (d == 0) {
+        cout << "Roots are real and same \n";
+        cout << -(double)b / (2 * a);
+
+    }
+    else // d < 0
+    {
+        cout << "Roots are complex \n";
+        cout << -(double)b / (2 * a) << " + i"
+             << sqrt_val / (2 * a) << "\n"
+             << -(double)b / (2 * a) << " - i"
+             << sqrt_val / (2 * a);
+    }
+}
+
+// Driver code
+int main()
+{
+    int a, b, c;
+
+    cout << "Podaj a:" << endl;
+    cin >> a;
+    cout << endl;
+
+    cout << "Podaj b:" << endl;
+    cin >> b;
+    cout << endl;
+
+    cout << "Podaj c:" << endl;
+    cin >> c;
+    cout << endl;
+
+
+    // Function call
+    findRoots(a, b, c);
+    return 0;
+}
